ef021efe3d03309213707d51a78dbef8b27a6954
Navarro-Mauricio <navarromauricio8@gmail.com>
2024-02-06 05:22:59 -0300

    second commit

--- src/componentes/CartWidget/CartWidget.jsx
+++ src/componentes/CartWidget/CartWidget.jsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { CarritoContext } from "../../context/CarritoContext";
 import { Link } from "react-router-dom";
-import '../Navbar/NavBar.css';
+import '../NavBar/NavBar';
 const CartWidget = () => {
     const {cantidadTotal} = useContext(CarritoContext);
 
